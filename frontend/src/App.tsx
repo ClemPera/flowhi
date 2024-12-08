24ae2f70aa412ae23d7003af9402982d10bd75f8
clemPera <clement.pera@gmail.com>
2024-12-08 17:49:55 -0500

    okay navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
 import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
     return (
         <>
-            yo
+            <div className="">
+            <nav className="flex bg-zinc-800 bg-opacity-40 mx-60 my-2 p-4 pb-5 rounded-2xl">
+                <div className="flex-1">
+                    <a href="/">
+                        [img] myapp
+                    </a>
+                </div>
+
+                <div className="flex-2">
+                    <a href="/login" className="ml-auto">
+                        Sign in
+                    </a>
+                </div>
+            </nav>
+            </div>
         </>
     )
 }

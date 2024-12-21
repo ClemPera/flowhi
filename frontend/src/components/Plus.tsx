8b50cd367e8eb9cf82ea4a90653fb7548ed6d884
clemPera <clement.pera@gmail.com>
2024-12-20 20:19:44 -0500

    tailwindJIT+hover

--- frontend/src/components/Plus.tsx
+++ frontend/src/components/Plus.tsx
@@ -3,7 +3,7 @@ import '../index.css'
 export default function Plus() {
     return (
         <div className="flex">
-            <button className="grow bg-zinc-800 mx-1 md:mx-24 xl:mx-60 my-2 p-4 py-10 rounded-xl">
+            <button className="grow bg-zinc-800 mx-1 md:mx-24 xl:mx-60 my-2 p-4 py-10 rounded-xl hover:bg-opacity-80">
                 <a className="font-bold text-2xl">+ Create</a>
             </button>
         </div>

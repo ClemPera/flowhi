b865a1f8067cac0f772595247a0e9b97ad862bc3
clemPera <clement.pera@gmail.com>
2024-12-29 17:55:55 -0500

    start to show from db

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,38 @@
 import './index.css'
+import { StrictMode, useState, useEffect } from 'react';
 import Plus from './components/Plus'
 import Scale from './components/Scale'
 
 function App() {
+    let [compo, setCompo] = useState([]);
+    
+    useEffect(() => {
+        fetch("http://localhost:3000/fields", {"method":"GET"})
+            .then(response => {
+                if(!response.ok) throw new Error("Issue fetching data from api (network response !ok)")
+                return response.json();
+            })
+            .then(data => {
+                setCompo(data);
+            })
+            .catch(error => console.error("Fetch error:", error)); // Handle errors
+
+        console.log(compo);
+    }, [])
+
+    let tmp = []
+    compo.forEach(comp => {
+        tmp.push(comp['id']);
+    });
+
     return (
         <>
-            <Scale n={8} />
-            <Plus />
+            <StrictMode>
+                <p>{JSON.stringify(compo)}</p>
+                <p>{tmp}</p>
+                <Scale n={8} />
+                <Plus />
+            </StrictMode>
         </>
     )
 }

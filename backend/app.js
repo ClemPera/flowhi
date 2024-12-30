bbee6e63eb4cf5a22c2091042fe55619da8d82f9
clemPera <clement.pera@gmail.com>
2024-12-30 17:16:31 -0500

    fix routing

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/data');
-var usersRouter = require('./routes/fields');
+var dataRouter = require('./routes/data');
+var fieldsRouter = require('./routes/fields');
 
 var app = express();
 const cors = require('cors')
@@ -28,8 +28,8 @@ let corsOptions = {
 app.use(cors(corsOptions))
 
 app.use('/', indexRouter);
-app.use('/data', usersRouter);
-app.use('/fields', usersRouter);
+app.use('/data', dataRouter);
+app.use('/fields', fieldsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

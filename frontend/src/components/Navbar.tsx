40238b2413fbadad025115a191c69bc903f070e5
clemPera <clement.pera@gmail.com>
2025-01-25 15:04:26 -0500

    favicon

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import '../index.css'
-import logo from '../assets/logo.svg'
+import logo from '../../public/logo.svg'
 
 export default function App() {
     return (
         <>
-            <nav className="flex bg-zinc-800 bg-opacity-40  md:mx-40 xl:mx-80 my-2 p-4 px-8 pb-5 rounded-2xl">
-                <a href="/" className=''>
+            <nav className="flex bg-zinc-800 bg-opacity-40 md:mx-40 xl:mx-80 my-2 p-4 px-8 pb-5 rounded-2xl">
+                <a href="/" className='-translate-y-0.5'>
                     <img className='h-8 w-8' src={logo} alt="website logo - sun with flow under (white)"/>
                 </a>
                 <div className="flex-grow ml-2">

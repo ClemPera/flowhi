476153af0baee89e0ae2e1351350bb4bd53b484e
clemPera <clement.pera@gmail.com>
2025-04-05 09:24:38 -0400

    time get DateTime ready handle

--- frontend/src/components/Time.tsx
+++ frontend/src/components/Time.tsx
@@ -2,6 +2,29 @@ import { motion } from "motion/react";
 import { useEffect, useRef, useState } from "react";
 
 export default function Time() {
+  let [total, setTotal] = useState(new Date(0,0,0,0,0,0))
+  let [count, setCount] = useState(1);
+  let [values, setValues] = useState<Array<Date>>([]);
+
+  useEffect(() => {
+    //TODO: Calculer le total and push to DB
+
+    //This is bad bc it count the value times:
+    // values.forEach(value => {
+    //   let hours = total.getHours() + value.getHours();
+    //   let minutes = total.getMinutes() + value.getMinutes();
+
+    //   console.log(hours+"h " + minutes + "m");
+
+    //   if (minutes >= 60) {
+    //     hours += Math.floor(minutes / 60);
+    //     minutes = minutes % 60;
+    //   }
+
+    //   setTotal(new Date(0,0,0,hours,minutes,0));
+    // });
+  }, [values])
+
   return (
     <div className="grid grid-rows-3 bg-zinc-800 mx-1 md:mx-24 xl:mx-60 my-2 p-4 pb-4 rounded-xl">
       <div className="relative grid grid-cols-12">
@@ -13,31 +36,42 @@ export default function Time() {
       <div className="place-content-center">
         <div className="flex place-content-center">
           {/* TODO:Ajouter un + et un input à chaque fois que celui d'avant est écrit + faire pour que l'input s'adapte au texte*/}
-          <Thing />
-           + 
-          <Thing />
-           + 
-          <Thing />
+          {(() => {
+            const elements = [];
+            for (let i = 0; i < count; i++) {
+              elements.push(<Thing key={i} id={i} values={values} setValues={setValues} count={count} setCount={setCount}/>);
+            }
+            return elements;
+          })()}
+          <p>total:{total.toTimeString()}</p>
         </div>
       </div>
     </div>
   );
 }
 
-function Thing(){
-  let [active, setActive] = useState(0);
+function Thing({id, values, setValues, count, setCount}: {id: number, values: Array<Date>, setValues: (v: Array<Date>) => void, count: number, setCount: (a: number) => void}){
+  let [active, setActive] = useState(-1);
   let [h1, setH1] = useState(0)
   let [h2, setH2] = useState(0)
   let [m1, setM1] = useState(0)
   let [m2, setM2] = useState(0)
+  let [counted, setCounted] = useState(false);
 
+  //TODO: Faire des check (pas dépasser 23h + pas dépasser 59 minutes)?
   useEffect(() => {
-    console.log("h:" + h1 + h2 + m1 + m2);
-    //TODO: Save les results dans db
+    if(!counted && (h1 != 0 || h2 != 0 || m1 != 0 || m2 != 0)){
+      setCount(count+1);
+      setCounted(true);
+    }
+
+    let newValues = [...values];
+    newValues[id] = new Date(0,0,0,parseInt(h1.toString()+h2.toString()),parseInt(m1.toString()+m2.toString()),0)
+    setValues(newValues);
+    console.log("write val w/:" + newValues);
     
   }, [h1,h2,m1,m2])
-
-  //TODO: Faire des check (pas dépasser 23h + pas dépasser 59 minutes)
+  
   return(
     <form className="flex flex-row bg-black rounded-xl p-2">
       <Input id={0} active={active} setActive={setActive} val={h1} setVal={setH1} />
